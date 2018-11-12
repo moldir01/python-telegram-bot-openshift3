383c6014fdb91cc8589975144a2ff6549422f0e1
moldir01 <44487510+moldir01@users.noreply.github.com>
2018-11-12 17:49:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import logging
-import random
-from queue import Queue
-from threading import Thread
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, ParseMode
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Filters, CallbackQueryHandler
 
@@ -15,16 +12,16 @@ def start(bot, update):
     """Send a message when the command /start is issued."""
     """update.message.reply_text('Welcome to the Test Bot! I will reply you what you will write me.')"""
     bot.send_message(chat_id=update.message.chat_id,
-                     text='<b>KAZGUU site</b>,<a href="http://kazguu.kz/ru/">KAZGUU</a>', parse_mode=ParseMode.HTML)
+                     text='<b> Привет от Акжан,Жансая,Жанбота,Молдир. Че вы хотите?</b>', parse_mode=ParseMode.HTML)
 
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
     # update.message.reply_text('You can get any help here.')
 
-    keyboardButtons = [[InlineKeyboardButton("Помощь", callback_data="1")],
-                       [InlineKeyboardButton("Примеры", callback_data="2")],
-                       [InlineKeyboardButton("Ссылка", url="http://google.com")]]
+    keyboardButtons = [[InlineKeyboardButton("скорая помощь", callback_data="1")],
+                       [InlineKeyboardButton("служба пожаратушения", callback_data="2")],
+                       [InlineKeyboardButton("полиция",callback_data="3")]]
     keyboard = InlineKeyboardMarkup(keyboardButtons)
     update.message.reply_text('Сделайте выбор:', reply_markup=keyboard)
 
@@ -32,42 +29,15 @@ def help(bot, update):
 def button(bot, update):
     query = update.callback_query
     if query.data == "1":
-        text = "You can choose any of the following actions: +, -, /, *"
+        text = "телефон 103"
     elif query.data == "2":
-        text = "3+4, 44-12, 43/2, 12*90"
+        text = "телефон 101"
+    elif query.date == "3":
+        text = "телефон 102"
     bot.editMessageText(text=text, chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
 
 
-def ar(bot, update):
-    random_num = random.randint(1, 100)
-    num_true = True
-    try:
-        num = int(update.message.text)
-        while num_true == True:
-            if random_num == num:
-                ar = 'Вы выиграли!'
-                num_true = False
-            elif random_num > num:
-                ar = 'Ваше число меньше моего. Попробуйте еще раз :)'
-            elif random_num < num:
-                ar = 'Ваше число больше моего. Попробуйте еще раз :)'
-        # ar = int(num) * 370
-    except (NameError, SyntaxError, ValueError):
-        ar = "Введите целое число"
-    num_true = True
-    bot.send_message(chat_id=update.message.chat_id, text=ar)
-
-
-"""def echo(bot, update):
-    update.message.reply_text('You said:  ' + update.message.text)
-"""
-
-"""def dolintenge(bot, update, args):
-    dollars = int(args)
-    tenge = dollars * 356
-    update.message.reply_text(text=tenge)
-"""
 
 
 def error(bot, update, error):
@@ -78,41 +48,22 @@ def error(bot, update, error):
 # Write your handlers here
 
 
-def setup(webhook_url=None):
-    """If webhook_url is not passed, run with long-polling."""
-    logging.basicConfig(level=logging.WARNING)
-    if webhook_url:
-        bot = Bot(TOKEN)
-        update_queue = Queue()
-        dp = Dispatcher(bot, update_queue)
-    else:
-        updater = Updater(TOKEN)  # Create the EventHandler and pass it your bot's token.
-        bot = updater.bot
-        dp = updater.dispatcher  # Get the dispatcher to register handlers
-        dp.add_handler(CommandHandler("start", start))  # on /start command answer in Telegram
-        dp.add_handler(CommandHandler("help", help))  # on /help command answer in Telegram
-        """dp.add_handler(CommandHandler("dolintenge", dolintenge))"""
-        dp.add_handler(CallbackQueryHandler(button))
-
-        # on noncommand i.e message - echo the message on Telegram
-        """dp.add_handler(MessageHandler(Filters.text, echo))"""
-        dp.add_handler(MessageHandler(Filters.text, ar))
+def setup():
+    updater = Updater(TOKEN)  # Create the EventHandler and pass it your bot's token.
+    bot = updater.bot
+    dp = updater.dispatcher  # Get the dispatcher to register handlers
+    dp.add_handler(CommandHandler("start", start))  # on /start command answer in Telegram
+    dp.add_handler(CommandHandler("help", help))  # on /help command answer in Telegram
+    dp.add_handler(CallbackQueryHandler(button))
 
         # log all errors
-        dp.add_error_handler(error)
-    # Add your handlers here
-    if webhook_url:
-        bot.set_webhook(webhook_url=webhook_url)
-        thread = Thread(target=dp.start, name='dispatcher')
-        thread.start()
-        return update_queue, bot
-    else:
-        bot.set_webhook()  # Delete webhook
-        updater.start_polling()  # Start the Bot
+    dp.add_error_handler(error)
+
+    updater.start_polling()  # Start the Bot
         """Run the bot until you press Ctrl-C or the process receives SIGINT,
         SIGTERM or SIGABRT. This should be used most of the time, since
         start_polling() is non-blocking and will stop the bot gracefully."""
-        updater.idle()
+    updater.idle()
 
 
 if __name__ == '__main__':
